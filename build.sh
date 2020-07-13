18b0a122b3d77f91064e7dc6248782359c111204
Dani Pragustia <danipragustia@disroot.org>
2020-07-13 11:18:55 +0700

    build: fix my silly bug and added temp folder to gitignore

--- build.sh
+++ build.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
+
 mkdir -p $(pwd)/_temp
 cd $(pwd)/_temp
 
 IFS=$'\n'
+VERSION=`node -p "require('../package.json').version"`
 
 electron-packager ../ --all --overwrite
 
-for f in $(find "$1" -mindepth 1 -maxdepth 1 -type d ); do
-	zip -r "$f" "$f"
+for f in $(find -mindepth 1 -maxdepth 1 -type d ); do
+	zip -r "$f-$VERSION.zip" "$f"
 done
